9bddfb1989664472dec65cabf030c8b22f44f8a4
Eli Moshe <moshebogo@gmail.com>
2023-05-07 05:24:35 -0400

    social links set up

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,5 +1,33 @@
 export default function About() {
+
+    const facebook = "/home/eli_moshe/Development/code/chayo/client/src/assets/facebook logo.jpg"
+    const twitter = "twitter"
+    const instagram = "instagram"
+    const pintrest = "pintrest"
+
     return (
-        <h1>About</h1>
+        <div>
+            <h1>About</h1>
+                        {/* facebook */}
+            <a href="https://www.facebook.com/cmchairstudio">
+            <img src={facebook} alt ="facebook-link"/>
+            </a>
+
+                         {/* twitter */}
+             <a href="">
+                <img src={twitter} alt="twitter-link"/>
+             </a>
+
+                         {/* instagram */}
+             <a href="">
+                <img src={instagram} alt="instagram-link"/>
+             </a>
+
+                         {/* pintrest */}
+             <a href="">
+                <img src={pintrest} alt="pintrest-link"/>
+             </a>
+        </div>
+
     )
 }
