0320293866a5722e001faed9b812aca337572cec
Eli Moshe <moshebogo@gmail.com>
2023-05-17 01:40:48 -0400

    centered

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,31 +1,35 @@
+import { image1, image2, image3, image4} from './images.js' 
+
 export default function About() {
 
     const facebook = "/home/eli_moshe/Development/code/chayo/client/src/assets/facebook logo.jpg"
     const twitter = "twitter"
     const instagram = "instagram"
     const pintrest = "pintrest"
+    const veyron = "./assets/veyron.jpeg"
+    const chiron  ="/home/eli_moshe/Development/code/chayo/client/src/assets/chiron.jpeg"
 
     return (
-        <div>
-            <h1>About</h1>
+        <div id="">
+            <h3>Chaya grew up with her mother styling hair and wigs for her clients from home. It was there that she started playing with her hair, and friends' hair, imitating the Updos she saw in her mother's hairstyle magazines, and recreating hairstyles from her favorite actresses like Mary Kate and Ashley Olsen. In 2009, Chaya started working as a hairstylist. Creating special occasion hairstyles for friends, and eventually, other women who discovered her work on Facebook. After reading Vogue, Harper's Bazaar, Allure, and Glamour Magazines, and reading an article about Celebrity Hairstylists, such as Ken Paves, and Chris Mcmillan, Chaya was inspired to go get her license. After completing 1,000 hours at a top cosmetology program, Aveda Institute New York, and winning a scholarship contest to enroll, Chaya received a New York State License to practice Cosmetology. Though her specialty is styling intricate updos for brides, and creating natural looking styles on wigs, Chaya's greatest strength is forming lasting bonds with everyone she works with. </h3>
                         {/* facebook */}
             <a href="https://www.facebook.com/cmchairstudio">
-            <img src={facebook} alt ="facebook-link"/>
+            <img class="center" src={image1} alt ="facebook-link"/>
             </a>
 
                          {/* twitter */}
              <a href="">
-                <img src={twitter} alt="twitter-link"/>
+                <img src={image2} alt="twitter-link"/>
              </a>
 
                          {/* instagram */}
              <a href="">
-                <img src={instagram} alt="instagram-link"/>
+                <img src={image3} alt="instagram-link"/>
              </a>
 
                          {/* pintrest */}
              <a href="">
-                <img src={pintrest} alt="pintrest-link"/>
+                <img src={image4} alt="pintrest-link"/>
              </a>
         </div>
 
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,27 +1,41 @@
 import { Link } from "react-router-dom"
+import { image1, image2, image3, image4} from './images.js' 
+
   
 export default function Header() {
 
-    const image1 = "/home/eli_moshe/Development/code/chayo/client/src/assets/image1.jpg"
-    const image2 = "/home/eli_moshe/Development/code/chayo/client/src/assets/image2.png"
-    const image3 = "/home/eli_moshe/Development/code/chayo/client/src/assets/image3.jpeg"
-    const image4 = "/home/eli_moshe/Development/code/chayo/client/src/assets/image4.png"
-
     return (
-        <div>
-            <h1>Header</h1>
+        <>
+            {/* <h1>Header</h1> */}
+        <div id="headerImages">
 
-          <Link to="/about">
-             <img src={image2} alt="hair"/>
-          </Link>
+                 <div class="container">
+                    <Link to="/about">
+                        <img class="image" src={image2} alt="hair"/>
+                        <div class="center">About</div>
+                    </Link>
+                 </div>
 
-          <Link to="/portfolio">
-             <img src={image3} alt="hair"/> 
-          </Link>
- 
-          <Link to="/photoshoot">
-            <img src={image4} alt="hair"/>
-         </Link>  
+                 <div class="container">
+                    <Link to="/portfolio">
+                        <img class="image" src={image3} alt="hair"/> 
+                        <div class="center">View Portfolio</div>
+                    </Link>
+                 </div>
+
+                 <div class="container">           
+                    <Link to="/photoshoot">
+                        <img class="image" src={image4} alt="hair"/>
+                        <div class="center">Phooshoots</div>
+                    </Link>  
+                    </div>   
+
+                 <div class="container">     
+                    <img class="image" src={image1} alt="hai"/>
+                    <div class="center">Stuff</div>
+                 </div>
+
         </div>
+        </>
     )
 }
