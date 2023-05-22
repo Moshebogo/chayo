44f2971b4d005439a22e58dedfbc4362b2db2e3a
Eli Moshe <moshebogo@gmail.com>
2023-05-22 06:45:15 -0400

    stuff

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -9,7 +9,7 @@ export default function About() {
     const veyron = "./assets/veyron.jpeg"
     const chiron  ="/home/eli_moshe/Development/code/chayo/client/src/assets/chiron.jpeg"
 
-    return (  
+    return (    
         <div id="">
             <h3>Chaya grew up with her mother styling hair and wigs for her clients from home. It was there that she started playing with her hair, and friends' hair, imitating the Updos she saw in her mother's hairstyle magazines, and recreating hairstyles from her favorite actresses like Mary Kate and Ashley Olsen. In 2009, Chaya started working as a hairstylist. Creating special occasion hairstyles for friends, and eventually, other women who discovered her work on Facebook. After reading Vogue, Harper's Bazaar, Allure, and Glamour Magazines, and reading an article about Celebrity Hairstylists, such as Ken Paves, and Chris Mcmillan, Chaya was inspired to go get her license. After completing 1,000 hours at a top cosmetology program, Aveda Institute New York, and winning a scholarship contest to enroll, Chaya received a New York State License to practice Cosmetology. Though her specialty is styling intricate updos for brides, and creating natural looking styles on wigs, Chaya's greatest strength is forming lasting bonds with everyone she works with. </h3>
                         {/* facebook */}
